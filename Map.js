c496bbe6846de44fe84fb61f1fe74cf370722af9
Erin Bergman <erin.e.bergman@gmail.com>
2016-10-04 15:59:25 -0400

    inital commit, changed over all look, added task bar to theleft of map, changed the text that displays in the drop bar.

---
+++ Map.js
@@ -0,0 +1,76 @@
+var globalMap;
+
+$(function() {
+
+    var MapFcns = {
+
+        loadSiteList: function() {
+            var airportList = $('#airport-list');
+            airportList.html('');
+            airportList.append('<option value=""></option>');
+            for (var i in sites) {
+                var newOption = $('<option value="' + sites[i].Code + '">' + sites[i].City + ": " + sites[i].FullSiteName + '</option>');
+                airportList.append(newOption);
+            }
+        },
+
+        siteListChange: function() {
+            var ctl = $(this),
+                airportCode = ctl.val();
+            if (airportCode) {
+                var currAirport = _.findWhere(sites, {
+                    Code: airportCode
+                });
+                $('#setting-code').text(currAirport.Code);
+                $('#setting-city').text(currAirport.City);
+                $('#setting-name').text(currAirport.FullSiteName);
+                $('#setting-lat').text(currAirport.Latitude);
+                $('#setting-long').text(currAirport.Longitude);
+
+                var marker = new google.maps.Marker({
+                    position: {
+                        lat: currAirport.Latitude,
+                        lng: currAirport.Longitude
+                    },
+                    map: globalMap,
+                    title: currAirport.Code
+                });
+            }
+        }
+
+
+    }
+
+
+    MapFcns.loadSiteList();
+    $('#airport-list').change(MapFcns.siteListChange);
+    $('#exercise-toggle').click(function() {
+        var toggleCtl = $(this),
+            toggleVal = toggleCtl.text();
+        if (toggleVal == '-') {
+            toggleCtl.text('+');
+            $('#exercise-instructions').hide();
+        } else {
+            toggleCtl.text('-');
+            $('#exercise-instructions').show();
+        }
+    });
+
+});
+
+
+
+function initMap() {
+    // Callback function to create a map object and specify the DOM element for display.
+    globalMap = new google.maps.Map(document.getElementById('airport-map'), {
+        center: {
+            lat: 42.2814,
+            lng: -83.7483
+        },
+        scrollwheel: true,
+        zoom: 6
+    });
+
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+}
