bfa99f16d67ea604247ee7fb6bcf8afe2cf29ed9
Erin Bergman <erin.e.bergman@gmail.com>
2016-10-04 18:29:49 -0400

    cleaned up look, added InfoWindow, remove pin, pan to pin

--- Map.js
+++ Map.js
@@ -1,4 +1,20 @@
 var globalMap;
+var marker;
+
+function initMap() {
+    // Callback function to create a map object and specify the DOM element for display.
+    globalMap = new google.maps.Map(document.getElementById('airport-map'), {
+        center: {
+            lat: 42.2814,
+            lng: -83.7483
+        },
+        scrollwheel: true,
+        zoom: 6
+    });
+
+
+
+}
 
 $(function() {
 
@@ -27,13 +43,49 @@ $(function() {
                 $('#setting-lat').text(currAirport.Latitude);
                 $('#setting-long').text(currAirport.Longitude);
 
-                var marker = new google.maps.Marker({
+                $airCode = currAirport.Code;
+                $airCity = currAirport.City;
+                $airState = currAirport.State;
+                $airSiteName = currAirport.FullSiteName;
+                $airLat = currAirport.Latitude;
+                $airLong = currAirport.Longitude;
+
+                var contentString = '<table>' +
+            '<tr>' +
+                '<th>Code</th>' +
+                '<th>City</th>' +
+                '<th>State</th>' +
+                '<th>Full Name</th>' +
+                '<th>Latitude</th>' +
+                '<th>Longitude</th>' +
+            '</tr>' +
+            '<tr>' +
+                '<td id="setting-code">' + $airCode + '</td>' +
+                '<td id="setting-city">' + $airCity + '</td>' +
+                '<td id="setting-state">' + $airState + '</td>' +
+                '<td id="setting-name">' + $airSiteName + '</td>' +
+                '<td id="setting-lat">' + $airLat + '</td>' +
+                '<td id="setting-long">' + $airLong + '</td>' +
+            '</tr>' +
+            
+        '</table>' +
+        '<input onclick="deleteMarkers();" type=button value="Delete Marker">';
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                marker = new google.maps.Marker({
                     position: {
                         lat: currAirport.Latitude,
                         lng: currAirport.Longitude
                     },
                     map: globalMap,
                     title: currAirport.Code
+                });
+                globalMap.panTo(marker.position);
+                marker.addListener('click', function() {
+                    infowindow.open(globalMap, marker);
                 });
             }
         }
@@ -60,17 +112,6 @@ $(function() {
 
 
 
-function initMap() {
-    // Callback function to create a map object and specify the DOM element for display.
-    globalMap = new google.maps.Map(document.getElementById('airport-map'), {
-        center: {
-            lat: 42.2814,
-            lng: -83.7483
-        },
-        scrollwheel: true,
-        zoom: 6
-    });
-
-    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-}
+function deleteMarkers() {
+    marker.setVisible(false);
+};
